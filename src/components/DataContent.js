33e49e786a219f40b1ec6f3b53f65266977dc3c3
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-07-15 20:21:45 -0700

    added data comp and table

---
+++ src/components/DataContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DataContent({ users }){
+    function formatDate(date) {
+        const dateArr
+    }
+
+    return (
+
+    )
+}
+
+export default DataContent;
---
+++ src/components/Table.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Table({ headings, users, handleSort }) {
+  return (
+    <div className="datatable">
+      <table id="table" className="table">
+        <thead>
+          <tr>
+            {headings.map(({ name, width }) => {
+              return (
+                <th
+                  className="col"
+                  key={name}
+                  style={{ width }}
+                  onClick={() => {
+                    handleSort(name.toLowerCase());
+                  }}
+                >
+                  {name}
+                  <span className="pointer"></span>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+
+        <DataBody users={users} />
+      </table>
+    </div>
+  );
+}
+
+export default Table;
